1739f7788cd398792c2a967146ea563b87afbf17
Pisoth567 <pisoth887@gmail.com>
2026-03-13 11:02:14 +0700

    Create Product Service.

--- src/main/java/co/istad/pisoth/fswd_sbapp/service/ProductService.java
+++ src/main/java/co/istad/pisoth/fswd_sbapp/service/ProductService.java
@@ -1,4 +1,8 @@
 package co.istad.pisoth.fswd_sbapp.service;
 
+import co.istad.pisoth.fswd_sbapp.dto.CreateProductRequest;
+import co.istad.pisoth.fswd_sbapp.dto.ProductResponse;
+
 public interface ProductService {
+    ProductResponse createNew(CreateProductRequest createProductRequest);
 }
